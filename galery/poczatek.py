078ae4d0e973b01a3a7196959481a1894da46781
drak <Thalion11>
2020-12-22 12:31:37 +0100

    wczorajszy

--- galery/poczatek.py
+++ galery/poczatek.py
@@ -1,8 +1,20 @@
 import os
 
-os.chdir('/home')
+pat = '/home'
+os.chdir(pat)
 scierzka = os.getcwd()
 l = list(os.listdir())
+print(l)
+count = 0
+for count in range(0, len(l)):
+    pat = pat+'/'+l[count]
+    count = 0
+    os.chdir(pat)
+    l = list(os.listdir())
+    print(pat)
+    print(l)
+
 if __name__ == '__main__':
     print(scierzka)
-    print(l)
+    # print (len(l))
+    # print(l)
