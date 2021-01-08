0a318ebdf73f8154394620481011a7310536a551
drak <Thalion11>
2021-01-08 15:15:52 +0100

    dziala find_pic (tx pawel)

--- galery/poczatek.py
+++ galery/poczatek.py
@@ -18,13 +18,13 @@ def what_is_what(pat):
     try:
         os.chdir(pat)
         ListOfAll = list(os.listdir())
+        for count in range(0, len(ListOfAll)):
+            if os.path.isfile(ListOfAll[count]):
+                file_list.append(ListOfAll[count])
+            else:
+                dir_list.append(ListOfAll[count])
     except:
         print('ERROR - Brak Dostępu')
-    for count in range(0, len(ListOfAll)):
-        if os.path.isfile(ListOfAll[count]):
-            file_list.append(ListOfAll[count])
-        else:
-            dir_list.append(ListOfAll[count])
 
 
 def change_path():
@@ -41,13 +41,16 @@ def find_pic(ext,files_list):
 
     count = 0
     # picture = str
-    fl = files_list
+    # fl = files_list
 
-
-for count in range(0,len(file_list)):
-    if fl[count].split('.')[1] == template:
-        print('jest'+str(fl[count]))
-    print(f"test"+{fl[count]})
+    print('testowanie')
+    print(file_list)
+    print(dir_list)
+    for count in range(0,len(files_list)):
+        if files_list[count].split('.')[-1] == ext:
+            print('testowanie-loop')
+            print(files_list[count])
+        # print(fl[count])
 
     # for count in range(0,len(file_list)):
     #     found = re.search(template, file_list[count])
@@ -67,6 +70,6 @@ if __name__ == '__main__':
 
     what_is_what(pat)
     find_pic(template,file_list)
-# print(dir_list)
+print('test3')
 print(file_list)
 print(os.getcwd())
